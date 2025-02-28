67fe2519978c7559f42144c0994dc82ec9cdcfe5
NitocrisFGO <112039828+NitocrisFGO@users.noreply.github.com>
2025-02-27 20:14:55 -0700

    Update Totoko_text_generation.py

--- Totoko_text_generation.py
+++ Totoko_text_generation.py
@@ -1,40 +1,52 @@
-# Load model directly
 from transformers import AutoModelForCausalLM, AutoTokenizer
-import re
 
-device = "cuda"
+model_name = "Qwen/Qwen2.5-7B-Instruct-1M"
 
-tokenizer = AutoTokenizer.from_pretrained("kxdw2580/Qwen2.5-3B-Instruct-Uncensored-Test")
-model = AutoModelForCausalLM.from_pretrained("kxdw2580/Qwen2.5-3B-Instruct-Uncensored-Test")
-
-model = model.to(device)
-
-
-prompt = "你知道什么叫，做爱吗？"
-
-messages = [{"role": "user", "content": prompt}]
-
-text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-
-model_inputs = tokenizer(
-    text=[text],
-    max_length=512,
-    truncation=True,
-    padding="max_length",
-    return_tensors="pt"
-).to(device)
-
-# Generate response with optimized parameters
-generated_ids = model.generate(**model_inputs, max_new_tokens=512)
-generated_ids_trimmed = [
-    out_ids[len(in_ids):] for in_ids, out_ids in zip(model_inputs.input_ids, generated_ids)
-]
-
-# Decode generated tokens to text
-output_text = tokenizer.batch_decode(
-    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
 )
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-cleaned_text = [re.sub(r"<.*?>", "", text).strip() for text in output_text]
 
-print(cleaned_text[0])
+def generate(prompt):
+    messages = [
+        {"role": "system", "content": "以莱昂纳多·达·芬奇的角色性格和说话风格回答。"},
+        {"role": "user", "content": prompt}
+    ]
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+    generated_ids = model.generate(
+        **model_inputs,
+        max_new_tokens=512
+    )
+    generated_ids = [
+        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+
+    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    return response
+
+
+state = 0
+
+save_path = "./qwen2.5-7b-instruct"
+
+
+model.save_pretrained(save_path)
+tokenizer.save_pretrained(save_path)
+
+while state == 0:
+    print("开始输入：")
+    prompt = input()
+    if prompt == "`":
+        state = 1
+    else:
+        response = generate(prompt)
+        print(response)
